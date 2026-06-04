912ce5128963cc2560479d9ffd256f5bbd5a7e2c
XERONBOT <xeronv2@gmail.com>
2026-06-04 20:31:55 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIJWltMdeQb4zp1c71146hHfv0SFUTcSg",
+  authDomain: "xeronate.firebaseapp.com",
+  projectId: "xeronate",
+  storageBucket: "xeronate.firebasestorage.app",
+  messagingSenderId: "82543259196",
+  appId: "1:82543259196:web:7c819c8b74e1931dd35d39",
+  measurementId: "G-RJTYMW9TD0"
+};
