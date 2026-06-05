5f72b4b1004f0377aa8a5ff63c3b6de6ae720a47
XERONBOT <xeronv2@gmail.com>
2026-06-05 23:05:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-function kirimDonasi(){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-const data = {
-nama: document.getElementById("nama").value,
-nominal: document.getElementById("nominal").value,
-pesan: document.getElementById("pesan").value,
-youtube: document.getElementById("youtube").value
+import {
+getFirestore,
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIJWltMdeQb4zp1c71146hHfv0SFUTcSg",
+  authDomain: "xeronate.firebaseapp.com",
+  projectId: "xeronate",
+  storageBucket: "xeronate.firebasestorage.app",
+  messagingSenderId: "82543259196",
+  appId: "1:82543259196:web:7c819c8b74e1931dd35d39",
+  measurementId: "G-RJTYMW9TD0"
 };
 
-localStorage.setItem(
-"donasi",
-JSON.stringify(data)
-);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-alert("Donasi berhasil disimpan!");
+window.kirimDonasi = async function () {
 
-}
+  const data = {
+    nama: document.getElementById("nama").value,
+    nominal: document.getElementById("nominal").value,
+    pesan: document.getElementById("pesan").value,
+    youtube: document.getElementById("youtube").value,
+    waktu: new Date().toISOString()
+  };
 
-function lihatData(){
+  try {
 
-const data =
-JSON.parse(
-localStorage.getItem("donasi")
-);
+    await addDoc(collection(db, "donasi"), data);
 
-alert(
-JSON.stringify(
-data,
-null,
-2
-)
-);
+    alert("Donasi berhasil disimpan!");
 
-}
+  } catch (e) {
+
+    alert("Error: " + e.message);
+
+  }
+};
+
+window.lihatData = function () {
+
+  alert("Data tersimpan di Firestore");
